ed26c7d0bc346c2cf6ebdecd49405654ae12c32d
OpenByte <development.openbyte@gmail.com>
2020-10-16 18:51:24 +0200

    Add reentrancy safe event processor

---
+++ WinEventHook/ReentrancySafeEventProcessor.cs
@@ -0,0 +1,56 @@
+﻿namespace WinEventHook {
+    /// <summary>
+    /// An event processor that queues incoming events and processes them sequentially avoiding reentrancy in hook functions.
+    /// </summary>
+    /// <typeparam name="T">The type of event data</typeparam>
+    public class ReentrancySafeEventProcessor<T> {
+
+        private bool _processing = false;
+        private readonly Queue<T> _eventQueue = new Queue<T>();
+        private readonly Action<T> _eventProcessor;
+
+        /// <summary>
+        /// Creates a new event processor with the specified event handler.
+        /// </summary>
+        /// <param name="eventProcessor"></param>
+        public ReentrancySafeEventProcessor(Action<T> eventProcessor) {
+            _eventProcessor = eventProcessor;
+        }
+
+        /// <summary>
+        /// Enqueues the given event and starts processing of queued events if necessary.
+        /// </summary>
+        /// <param name="eventData">An object representing the event</param>
+        public void EnqueueAndProcess(T eventData) {
+            // check if already executing
+            if (_processing) {
+                // some call already reached processing.
+                _eventQueue.Enqueue(eventData);
+                return;
+            }
+
+            // first call, so we start executing
+            _processing = true;
+
+            try {
+                // process own event data
+                _eventProcessor(eventData);
+
+                // process queued events
+                while (_eventQueue.TryDequeue(out var data)) {
+                    _eventProcessor(data);
+                }
+            } finally {
+                // stop executing
+                _processing = false;
+            }
+        }
+
+        /// <summary>
+        /// Flush the current event queue.
+        /// </summary>
+        public void FlushQueue() {
+            _eventQueue.Clear();
+        }
+    }
+}
